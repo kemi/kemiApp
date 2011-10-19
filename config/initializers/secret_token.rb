17ad0aa16fad03bf70d32a3b8968cb777a2112c3
kemi <elsiekem08@yahoo.com>
2011-10-19 11:16:16 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KemiApp::Application.config.secret_token = '27e4857f396b93ae229bc4ed4e4c0cd6aff6a9dc954fdbc2e6838e3ec97177cbf779594d7c018afe42f31daa7970ebc9bed97bfe61b6e2acef7c4041c61bdf05'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+KemiApp::Application.config.session_store :cookie_store, key: '_kemiApp_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# KemiApp::Application.config.session_store :active_record_store
